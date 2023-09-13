41453a73ae4912f4c8074e6a07d542e2a1ae9c8c
Wahab <wahabrider984@gmail.com>
2023-09-13 02:39:25 -0700

    image document

---
+++ lib/view/scanimages/controller.dart
@@ -0,0 +1,8 @@
+import 'package:amc_management/view/scanimages/state.dart';
+import 'package:get/get.dart';
+
+class ScanImageController extends GetxController{
+
+  final state = ScanImageState();
+
+}
---
+++ lib/view/scanimages/state.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_document_scanner/flutter_document_scanner.dart';
+
+class ScanImageState {
+
+
+  final Doccontroller = DocumentScannerController();
+
+
+}
