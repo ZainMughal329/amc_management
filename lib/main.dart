178c0310ab7da46ef530b667b08509d2f2efd4a7
Wahab <wahabrider984@gmail.com>
2023-11-25 14:04:30 -0800

    session Colors

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class _MyAppState extends State<MyApp> {
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
           ),
-          initialRoute: RouteNames.homeview,
+          initialRoute: RouteNames.splashview,
           getPages:AppPages.routes,
         );
       },
